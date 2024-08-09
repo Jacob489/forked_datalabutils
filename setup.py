2e6d63fdcaeb42ada4189e763e87ce4de86b5a0e
Jake Smith <truenorthproductions87@gmail.com>
2024-08-09 04:37:28 -0700

    name change

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='astrodatautils',
+    name='astrodatalab',
     version='0.1.0',
     author='Jacob Nowack',
     author_email='truenorthproductions87@gmail.com',
